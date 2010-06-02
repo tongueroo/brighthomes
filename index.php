a915c00accff11d25d8e8d48a154866236601631
Tung Nguyen <tongueroo@gmail.com>
2010-06-02 16:58:32 -0700

    init commit

---
+++ index.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Bright Homes</title>
+<link href="scripts/base.css" rel="stylesheet" type="text/css" />
+<script type="text/javascript">
+<!--
+function MM_preloadImages() { //v3.0
+  var d=document; if(d.images){ if(!d.MM_p) d.MM_p=new Array();
+    var i,j=d.MM_p.length,a=MM_preloadImages.arguments; for(i=0; i<a.length; i++)
+    if (a[i].indexOf("#")!=0){ d.MM_p[j]=new Image; d.MM_p[j++].src=a[i];}}
+}
+
+function MM_swapImgRestore() { //v3.0
+  var i,x,a=document.MM_sr; for(i=0;a&&i<a.length&&(x=a[i])&&x.oSrc;i++) x.src=x.oSrc;
+}
+
+function MM_findObj(n, d) { //v4.01
+  var p,i,x;  if(!d) d=document; if((p=n.indexOf("?"))>0&&parent.frames.length) {
+    d=parent.frames[n.substring(p+1)].document; n=n.substring(0,p);}
+  if(!(x=d[n])&&d.all) x=d.all[n]; for (i=0;!x&&i<d.forms.length;i++) x=d.forms[i][n];
+  for(i=0;!x&&d.layers&&i<d.layers.length;i++) x=MM_findObj(n,d.layers[i].document);
+  if(!x && d.getElementById) x=d.getElementById(n); return x;
+}
+
+function MM_swapImage() { //v3.0
+  var i,j=0,x,a=MM_swapImage.arguments; document.MM_sr=new Array; for(i=0;i<(a.length-2);i+=3)
+   if ((x=MM_findObj(a[i]))!=null){document.MM_sr[j++]=x; if(!x.oSrc) x.oSrc=x.src; x.src=a[i+2];}
+}
+//-->
+</script>
+</head>
+
+<body onload="MM_preloadImages('images/cbt_fallsb.png','images/nav_01.gif','images/nav_02.gif','images/nav_03.gif','images/cbt_tbellab.png','images/cbt_bonitab.png','images/cbt_cimarronb.png','images/nav_04.gif','images/nav_05.gif')">
+<div id="container">
+	<div id="header">
+    	<div id="logo">
+        <a href="#" title="Bright Homes"><img src="images/spacer.gif" alt="" width="162" height="99" border="0" /></a>
+      <h2>Bright Homes</h2></div>
+      
+      
+      	<div id="header_right">
+       	  <div id="topnav">
+            	<div id="arrow"><img src="images/contact.png" width="16" height="16" /></div>
+           	<div id="text">Request More Info | Contact Us</div>
+          </div>
+             <div class="clearfix"></div>
+        </div>
+  
+ 
+  </div>
+      <div class="clearfix"></div>
+
+    
+  <div id="nav">
+  
+  		<ul class="white">
+	    		<li><a href="#" title="home" class="current"><span>Navigation 1</span></a></li>
+	    		<li><a href="#" title="products"><span>Navigation 2</span></a></li>
+	    		<li><a href="#" title="blog"><span>Navigation 3</span></a></li>
+	    		<li><a href="#" title="contact">Navigation 4</a></li>
+        		<li><a href="#" title="contact">Navigation 5</a></li>
+  		</ul>
+  
+  
+  
+  </div>
+
+
+
+	<div id="headerimage"><img src="images/headerhome.jpg" /></div>
+    
+    
+    
+<div id="maincontent">
+    	<div id="leftcontent">
+        	<p class="maincopy">Lutat augiam, vendiam etummodipit lut in er sit adigna faci exer sumsan vel eril dolore ming ex ea faciliquis nulput nulla faci bla feummy nonsecte tisim iureet amet, vel ulput dolore dolore ming eros dolortin exercip etummy nullupt ationse dit lor ilismol oboreet nulluptat. Delenit alisim do odit ad tat num.</p>
+      </div>
+      
+          	<div id="middlecontent"><a href="secondary"><img src="images/cbt_falls.png" border="0" id="Image6" onmouseover="MM_swapImage('Image6','','images/cbt_fallsb.png',1)" onmouseout="MM_swapImgRestore()" /></a>
+              <a href="#"><img src="images/cbt_tbella.png" alt="Terra Bella" border="0" id="Image7" onmouseover="MM_swapImage('Image7','','images/cbt_tbellab.png',1)" onmouseout="MM_swapImgRestore()" /></a>
+              <a href="#"><img src="images/cbt_bonita.png" alt="Bonita Ranch" border="0" id="Image8" onmouseover="MM_swapImage('Image8','','images/cbt_bonitab.png',1)" onmouseout="MM_swapImgRestore()" /></a>
+            <img src="images/cbt_cimarron.png" alt="Cimarron" border="0" id="Image9" onmouseover="MM_swapImage('Image9','','images/cbt_cimarronb.png',1)" onmouseout="MM_swapImgRestore()" />    </div>
+            
+            
+    <div id="rightcontent"></div>
+    
+    
+             <div class="clearfix"></div>
+  </div>
+    
+     <div id="footer">
+<div id="copyright">© 2010 Bright Homes<br />
+Site Designed by <a href="http://www.robertanthony.com" target="_blank">Robert Anthony</a></div>
+
+</div>
+
+
+
+</div>
+
+
+
+
+
+
+</body>
+</html>
