94c7cd314c08b1eeea70b5623c29dff7224664cf
Tung Nguyen <tongueroo@gmail.com>
2010-08-11 14:49:56 -0700

    fix nav slider code, remove ununsed examples

--- mpi/js/example.js
+++ mpi/js/example.js
@@ -7,18 +7,28 @@ $(function(){
     
     $mainNav2.append("<li id='magic-line-two'></li>");
     
+    var $menu_li, $menu_a, has_original;
+    
+    // has original item
+    if ($(".current_page_item_two a").length > 0) {
+      $menu_li = $(".current_page_item_two");
+      $menu_a = $(".current_page_item_two a");
+      has_original = true;
+    }
     // since all the menu items are the same width, can use any of them to do the width calculations
-    var $any_menu_li = $('#example-two li:first');
-    var $any_menu_a  = $('#example-two li:first a');
+    else {
+      $menu_li = $('#example-two li:first');
+      $menu_a  = $('#example-two li:first a');
+    }
     
     var $magicLineTwo = $("#magic-line-two");
     $magicLineTwo
-        .width($any_menu_li.width())
+        .width($menu_li.width())
         .height($mainNav2.height())
-        .css("left", $any_menu_a.position().left)
-        .data("origLeft", $any_menu_a.position().left)
+        .css("left", $menu_a.position().left)
+        .data("origLeft", $menu_a.position().left)
         .data("origWidth", $magicLineTwo.width())
-        .data("origColor", $any_menu_a.attr("rel"));
+        .data("origColor", $menu_a.attr("rel"));
 
 		function highlight(element) {
 		  $el = $(element);
@@ -34,11 +44,17 @@ $(function(){
     $("#example-two li").find("a").hover(function() {
       highlight(this);
     }, function() {
+      if (has_original) {
+        // go back go original hover
         $magicLineTwo.stop().animate({
             left: $magicLineTwo.data("origLeft"),
             width: $magicLineTwo.data("origWidth"),
             backgroundColor: $magicLineTwo.data("origColor")
         });    
+      } 
+      else {
+        $magicLineTwo.hide();
+      }
     });
 
     // preload the highlight based on the .current_page_item_two class
