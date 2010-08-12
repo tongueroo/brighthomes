40ab3434a5532873c71e88993aadad3bc19527bf
Tung Nguyen <tongueroo@gmail.com>
2010-08-12 09:45:25 -0700

    phuongs changes: ofswireless

---
+++ OFSwireless/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Untitled Document</title>
+<style type="text/css">
+<!--
+#background {
+    width: 100%; 
+    height: 100%; 
+    position: absolute; 
+    left: 0px; 
+    top: 0px; 
+    z-index: 0;
+}
+
+.stretch {
+    width:100%;
+    height:100%;
+}
+#wrap {
+	width: 600px;
+	margin-right: auto;
+	margin-left: auto;
+}
+
+
+
+-->
+</style>
+</head>
+
+<body>
+<div id="background">
+    <img src="images/waterbg.jpg" class="stretch" alt="" />
+</div>
+<div id="wrap">
+<img src="images/wireless_logo.png" alt="Oakland/Alameda Ferry" />
+</div>
+
+</body>
+</html>
